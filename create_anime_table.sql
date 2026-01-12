a3e9fecc6281e710302f745fe44ac8ebd2ef1a63
Ramil124 <119865381+Ramil124@users.noreply.github.com>
2026-01-12 22:22:09 +0400

    Add files via upload

---
+++ create_anime_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS anime(
+    mal_id INTEGER,
+    title_english TEXT,
+    type TEXT,
+    status TEXT,
+    synopsis TEXT, 
+    episodes INTEGER,
+    duration_minutes INTEGER,
+    rank  NUMERIC,
+    score NUMERIC,
+    rating TEXT,
+    broadcast_weekday TEXT,
+    broadcast_time TEXT,
+    from_date DATE
+)
